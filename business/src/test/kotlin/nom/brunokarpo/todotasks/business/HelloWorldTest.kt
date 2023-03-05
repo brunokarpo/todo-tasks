973c83dbcd9aa0f5bb353de5672b10f0d0fd13af
Bruno Nogueira <ti.brunonogueira@gmail.com>
2023-03-05 14:52:56 -0300

    Configured Junit5 test

---
+++ business/src/test/kotlin/nom/brunokarpo/todotasks/business/HelloWorldTest.kt
@@ -0,0 +1,12 @@
+package nom.brunokarpo.todotasks.business
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertTrue
+
+class HelloWorldTest {
+
+    @Test
+    fun name() {
+        assertTrue { true }
+    }
+}
