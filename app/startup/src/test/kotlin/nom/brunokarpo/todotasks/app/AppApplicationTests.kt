b6b8fe9d2a8ca82b1b96218bc4e7fd77183c410b
Bruno Nogueira <ti.brunonogueira@gmail.com>
2023-03-05 22:08:55 -0300

    Created spring boot module

---
+++ app/startup/src/test/kotlin/nom/brunokarpo/todotasks/app/AppApplicationTests.kt
@@ -0,0 +1,13 @@
+package nom.brunokarpo.todotasks.app
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AppApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
