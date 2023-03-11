484c657b48a217e1d7afe13bd4dcbd077a8903f8
Bruno Nogueira <ti.brunonogueira@gmail.com>
2023-03-11 00:18:55 -0300

    Implemented the tokenService to generate the JWT token
    Signed-off-by: Bruno Nogueira <ti.brunonogueira@gmail.com>

--- app/api/src/main/kotlin/nom/brunokarpo/todotasks/app/api/security/TokenService.kt
+++ app/api/src/main/kotlin/nom/brunokarpo/todotasks/app/api/security/TokenService.kt
@@ -1,12 +1,24 @@
 package nom.brunokarpo.todotasks.app.api.security
 
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
 import nom.brunokarpo.todotasks.app.api.dto.UserDetailsDTO
 import org.springframework.stereotype.Service
+import java.time.LocalDateTime
+import java.time.ZoneOffset
 
 @Service
 class TokenService {
     fun generateToken(user: UserDetailsDTO): String {
-        TODO("Not yet implemented")
+        return JWT.create()
+            .withIssuer("TODO_TASKS")
+            .withSubject(user.username)
+            .withClaim("id", user.username)
+            .withExpiresAt(
+                LocalDateTime.now()
+                    .plusMinutes(10)
+                    .toInstant(ZoneOffset.UTC)
+            ).sign(Algorithm.HMAC512("muito-dificil-de-descobrir"))
     }
 
 }
