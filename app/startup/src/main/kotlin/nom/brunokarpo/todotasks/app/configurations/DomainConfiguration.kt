88e32772478754cd36bd16381108e64ff0b067c1
Bruno Nogueira <ti.brunonogueira@gmail.com>
2023-03-11 01:13:42 -0300

    Implementing the UserProvider
    Signed-off-by: Bruno Nogueira <ti.brunonogueira@gmail.com>

--- app/startup/src/main/kotlin/nom/brunokarpo/todotasks/app/configurations/DomainConfiguration.kt
+++ app/startup/src/main/kotlin/nom/brunokarpo/todotasks/app/configurations/DomainConfiguration.kt
@@ -1,6 +1,5 @@
 package nom.brunokarpo.todotasks.app.configurations
 
-import nom.brunokarpo.todotasks.domain.model.User
 import nom.brunokarpo.todotasks.domain.repository.TaskRepository
 import nom.brunokarpo.todotasks.domain.repository.UserRepository
 import nom.brunokarpo.todotasks.domain.service.TaskService
@@ -8,7 +7,6 @@ import nom.brunokarpo.todotasks.domain.service.UserProvider
 import nom.brunokarpo.todotasks.domain.service.UserService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import java.util.UUID
 
 @Configuration
 class DomainConfiguration {
@@ -22,17 +20,4 @@ class DomainConfiguration {
     fun userService(userRepository: UserRepository): UserService {
         return UserService(userRepository = userRepository)
     }
-
-    @Bean
-    fun userProvider(): UserProvider = object: UserProvider {
-        override fun getUserFromSession(): User {
-            return User(
-                id = UUID.fromString("c1f0018e-cc53-49b1-a1b9-9bb8a017d2e2"),
-                name = "Dummy User",
-                email = "dummy@email"
-            )
-        }
-
-    }
-
 }
